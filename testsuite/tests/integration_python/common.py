44f2aac506946517cc37eb60fdd4bb0991485b3c
Julian Hofer <julianhofer@gnome.org>
2025-05-27 16:50:07 +0200

    Initial commit

---
+++ testsuite/tests/integration_python/common.py
@@ -0,0 +1,166 @@
+from contextlib import contextmanager
+from enum import IntEnum
+from pathlib import Path
+import platform
+import subprocess
+import os
+from typing import Generator
+
+from rattler import Platform
+
+PIXI_VERSION = "0.47.0"
+
+
+ALL_PLATFORMS = '["linux-64", "osx-64", "osx-arm64", "win-64", "linux-ppc64le", "linux-aarch64"]'
+
+CURRENT_PLATFORM = str(Platform.current())
+
+EMPTY_BOILERPLATE_PROJECT = f"""
+[workspace]
+name = "test"
+channels = []
+platforms = ["{CURRENT_PLATFORM}"]
+"""
+
+
+class ExitCode(IntEnum):
+    SUCCESS = 0
+    FAILURE = 1
+    INCORRECT_USAGE = 2
+
+
+class Output:
+    command: list[Path | str]
+    stdout: str
+    stderr: str
+    returncode: int
+
+    def __init__(self, command: list[Path | str], stdout: str, stderr: str, returncode: int):
+        self.command = command
+        self.stdout = stdout
+        self.stderr = stderr
+        self.returncode = returncode
+
+    def __str__(self) -> str:
+        return f"command: {self.command}"
+
+
+def verify_cli_command(
+    command: list[Path | str],
+    expected_exit_code: ExitCode = ExitCode.SUCCESS,
+    stdout_contains: str | list[str] | None = None,
+    stdout_excludes: str | list[str] | None = None,
+    stderr_contains: str | list[str] | None = None,
+    stderr_excludes: str | list[str] | None = None,
+    env: dict[str, str] | None = None,
+    cwd: str | Path | None = None,
+    reset_env: bool = False,
+) -> Output:
+    base_env = {} if reset_env else dict(os.environ)
+    complete_env = base_env if env is None else base_env | env
+    # Set `NO_GRAPHICS` to avoid to have miette splitting up lines
+    complete_env |= {"NO_GRAPHICS": "1"}
+
+    process = subprocess.run(
+        command,
+        capture_output=True,
+        env=complete_env,
+        cwd=cwd,
+    )
+    # Decode stdout and stderr explicitly using UTF-8
+    stdout = process.stdout.decode("utf-8", errors="replace")
+    stderr = process.stderr.decode("utf-8", errors="replace")
+    returncode = process.returncode
+    output = Output(command, stdout, stderr, returncode)
+    print(f"command: {command}, stdout: {stdout}, stderr: {stderr}, code: {returncode}")
+    assert returncode == expected_exit_code, (
+        f"Return code was {returncode}, expected {expected_exit_code}, stderr: {stderr}"
+    )
+
+    if stdout_contains:
+        if isinstance(stdout_contains, str):
+            stdout_contains = [stdout_contains]
+        for substring in stdout_contains:
+            assert substring in stdout, f"'{substring}'\n not found in stdout:\n {stdout}"
+
+    if stdout_excludes:
+        if isinstance(stdout_excludes, str):
+            stdout_excludes = [stdout_excludes]
+        for substring in stdout_excludes:
+            assert substring not in stdout, (
+                f"'{substring}'\n unexpectedly found in stdout:\n {stdout}"
+            )
+
+    if stderr_contains:
+        if isinstance(stderr_contains, str):
+            stderr_contains = [stderr_contains]
+        for substring in stderr_contains:
+            assert substring in stderr, f"'{substring}'\n not found in stderr:\n {stderr}"
+
+    if stderr_excludes:
+        if isinstance(stderr_excludes, str):
+            stderr_excludes = [stderr_excludes]
+        for substring in stderr_excludes:
+            assert substring not in stderr, (
+                f"'{substring}'\n unexpectedly found in stderr:\n {stderr}"
+            )
+
+    return output
+
+
+def bat_extension(exe_name: str) -> str:
+    if platform.system() == "Windows":
+        return exe_name + ".bat"
+    else:
+        return exe_name
+
+
+def exec_extension(exe_name: str) -> str:
+    if platform.system() == "Windows":
+        return exe_name + ".exe"
+    else:
+        return exe_name
+
+
+def is_binary(path: Path) -> bool:
+    textchars = bytearray({7, 8, 9, 10, 12, 13, 27} | set(range(0x20, 0x100)) - {0x7F})
+    with open(path, "rb") as f:
+        return bool(f.read(2048).translate(None, textchars))
+
+
+def pixi_dir(project_root: Path) -> Path:
+    return project_root.joinpath(".pixi")
+
+
+def default_env_path(project_root: Path) -> Path:
+    return pixi_dir(project_root).joinpath("envs", "default")
+
+
+def repo_root() -> Path:
+    return Path(__file__).parents[2]
+
+
+def current_platform() -> str:
+    return str(Platform.current())
+
+
+def get_manifest(directory: Path) -> Path:
+    pixi_toml = directory / "pixi.toml"
+    pyproject_toml = directory / "pyproject.toml"
+
+    if pixi_toml.exists():
+        return pixi_toml
+    elif pyproject_toml.exists():
+        return pyproject_toml
+    else:
+        raise ValueError("Neither pixi.toml nor pyproject.toml found")
+
+
+@contextmanager
+def cwd(path: str | Path) -> Generator[None, None, None]:
+    oldpwd = os.getcwd()
+    os.chdir(path)
+    try:
+        yield
+    finally:
+        os.chdir(oldpwd)
---
+++ testsuite/tests/integration_python/conftest.py
@@ -0,0 +1,129 @@
+from pathlib import Path
+
+import pytest
+
+from .common import exec_extension
+
+
+def pytest_addoption(parser: pytest.Parser) -> None:
+    parser.addoption(
+        "--pixi-build",
+        action="store",
+        default="release",
+        help="Specify the pixi build type (e.g., release or debug)",
+    )
+
+
+@pytest.fixture
+def pixi(request: pytest.FixtureRequest) -> Path:
+    # pixi_build = request.config.getoption("--pixi-build")
+    # pixi_path = Path(__file__).parent.joinpath(f"../../target/pixi/{pixi_build}/pixi")
+    # return Path(exec_extension(str(pixi_path)))
+    return Path("pixi")
+
+
+@pytest.fixture
+def tmp_pixi_workspace(tmp_path: Path) -> Path:
+    pixi_config = """
+# Reset to defaults
+default-channels = ["conda-forge"]
+shell.change-ps1 = true
+tls-no-verify = false
+detached-environments = false
+pinning-strategy = "semver"
+
+[concurrency]
+downloads = 50
+
+[experimental]
+use-environment-activation-cache = false
+
+# Enable sharded repodata
+[repodata-config."https://prefix.dev/"]
+disable-sharded = false
+"""
+    dot_pixi = tmp_path.joinpath(".pixi")
+    dot_pixi.mkdir()
+    dot_pixi.joinpath("config.toml").write_text(pixi_config)
+    return tmp_path
+
+
+@pytest.fixture
+def test_data() -> Path:
+    return Path(__file__).parents[1].joinpath("data").resolve()
+
+
+@pytest.fixture
+def pypi_data(test_data: Path) -> Path:
+    """
+    Returns the pixi pypi test data
+    """
+    return test_data.joinpath("pypi")
+
+
+@pytest.fixture
+def pixi_tomls(test_data: Path) -> Path:
+    """
+    Returns the pixi pypi test data
+    """
+    return test_data.joinpath("pixi_tomls")
+
+
+@pytest.fixture
+def mock_projects(test_data: Path) -> Path:
+    return test_data.joinpath("mock-projects")
+
+
+@pytest.fixture
+def channels(test_data: Path) -> Path:
+    return test_data.joinpath("channels", "channels")
+
+
+@pytest.fixture
+def dummy_channel_1(channels: Path) -> str:
+    return channels.joinpath("dummy_channel_1").as_uri()
+
+
+@pytest.fixture
+def dummy_channel_2(channels: Path) -> str:
+    return channels.joinpath("dummy_channel_2").as_uri()
+
+
+@pytest.fixture
+def multiple_versions_channel_1(channels: Path) -> str:
+    return channels.joinpath("multiple_versions_channel_1").as_uri()
+
+
+@pytest.fixture
+def non_self_expose_channel_1(channels: Path) -> str:
+    return channels.joinpath("non_self_expose_channel_1").as_uri()
+
+
+@pytest.fixture
+def non_self_expose_channel_2(channels: Path) -> str:
+    return channels.joinpath("non_self_expose_channel_2").as_uri()
+
+
+@pytest.fixture
+def virtual_packages_channel(channels: Path) -> str:
+    return channels.joinpath("virtual_packages").as_uri()
+
+
+@pytest.fixture
+def shortcuts_channel_1(channels: Path) -> str:
+    return channels.joinpath("shortcuts_channel_1").as_uri()
+
+
+@pytest.fixture
+def post_link_script_channel(channels: Path) -> str:
+    return channels.joinpath("post_link_script_channel").as_uri()
+
+
+@pytest.fixture
+def deno_channel(channels: Path) -> str:
+    return channels.joinpath("deno_channel").as_uri()
+
+
+@pytest.fixture
+def completions_channel_1(channels: Path) -> str:
+    return channels.joinpath("completions_channel_1").as_uri()
