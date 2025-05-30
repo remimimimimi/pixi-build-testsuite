913a44735c2907881eab5bcec78ed38cba1927eb
Julian Hofer <julianhofer@gnome.org>
2025-05-30 14:23:35 +0200

    Only run fast tests on CI for now

--- testsuite/tests/integration_python/test_examples.py
+++ testsuite/tests/integration_python/test_examples.py
@@ -6,7 +6,6 @@ import pytest
 from .common import get_manifest, repo_root, verify_cli_command
 
 
-@pytest.mark.skip(reason="Let's skip them for now, and only run them on main in the long run")
 @pytest.mark.slow
 @pytest.mark.parametrize(
     "pixi_project",
