6e7b6c3aa3f9e8bd6b5f932820cd4a0e22e2c9fc
Julian Hofer <julianhofer@gnome.org>
2025-06-03 09:55:02 +0200

    Remove python3.14 example

--- examples/python3.14/recipe/build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# shellcheck disable=SC2086
-./configure --prefix=${PREFIX} \
-    --oldincludedir=${BUILD_PREFIX}/${HOST}/sysroot/usr/include \
-    --enable-shared
-
-make -j${CPU_COUNT}
-make install
