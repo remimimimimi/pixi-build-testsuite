a799c6dd5930a4150bb749b484611256166f6b56
Julian Hofer <julianhofer@gnome.org>
2025-07-04 13:33:29 +0200

    There's no need to set that env var anymore

--- testsuite/tests/integration_python/test_backends.py
+++ testsuite/tests/integration_python/test_backends.py
@@ -18,9 +18,6 @@ from .common import get_manifest, repo_root, verify_cli_command
     ],
 )
 def test_pixi_minimal_backend(pixi_project: Path, pixi: Path, tmp_pixi_workspace: Path) -> None:
-    env = {
-        "PIXI_CACHE_DIR": str(tmp_pixi_workspace.joinpath("pixi_cache")),
-    }
     # Remove existing .pixi folders
     shutil.rmtree(pixi_project.joinpath(".pixi"), ignore_errors=True)
 
@@ -33,6 +30,5 @@ def test_pixi_minimal_backend(pixi_project: Path, pixi: Path, tmp_pixi_workspace
     # Install the environment
     verify_cli_command(
         [pixi, "run", "--locked", "--manifest-path", manifest, "start"],
-        env=env,
         stdout_contains="Build backend works",
     )
