d075278168eb2687e30cd1db545ac45451e004e3
remimimimimi <remimimimimi@protonmail.com>
2025-10-03 11:57:45 +0300

    fix lints

--- tests/data/pixi_build/ros-workspace/src/talker-py/setup.py
+++ tests/data/pixi_build/ros-workspace/src/talker-py/setup.py
@@ -1,25 +1,24 @@
 from setuptools import find_packages, setup
 
-package_name = 'talker_py'
+package_name = "talker_py"
 
 setup(
     name=package_name,
-    version='0.0.0',
-    packages=find_packages(exclude=['test']),
+    version="0.0.0",
+    packages=find_packages(exclude=["test"]),
     data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
-        ('share/' + package_name, ['package.xml']),
+        ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
+        ("share/" + package_name, ["package.xml"]),
     ],
-    install_requires=['setuptools'],
+    install_requires=["setuptools"],
     zip_safe=True,
-    maintainer='rubenarts',
-    maintainer_email='ruben.arts@hotmail.com',
-    description='TODO: Package description',
-    license='TODO: License declaration',
+    maintainer="rubenarts",
+    maintainer_email="ruben.arts@hotmail.com",
+    description="TODO: Package description",
+    license="TODO: License declaration",
     entry_points={
-        'console_scripts': [
-            'talker = talker_py.talker:main',
+        "console_scripts": [
+            "talker = talker_py.talker:main",
         ],
     },
 )
